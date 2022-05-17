4ac69cae6cd8364d59e67b86d04381ddfe139dea
ANVRRT <albertonavarreteramirez@gmail.com>
2022-05-16 20:04:57 -0500

    Added resume function?

--- testproject1/src/components/ResumeComponent.vue
+++ testproject1/src/components/ResumeComponent.vue
@@ -1,30 +1,21 @@
 <template>
-  <v-row  align="center" justify="center" >
-    <v-col cols="12" md="6">
-      <p class="blueTec--text text-center text-h4 font-weight-bold">INFORMACIÓN DEL ALUMNO</p>
-      <div class="text-center">
-        <v-avatar class="rounded-circle mb-5" size="164">
-          <v-img src="https://firebasestorage.googleapis.com/v0/b/fir-ideentest.appspot.com/o/Perfil%2Fbond.jpeg?alt=media&token=02bc0f1d-82b8-468b-bb75-52b78e1d5d3d"></v-img>
-        </v-avatar>
-      </div>
-      <v-row align="center" justify="center" justify-md="start" >
-        <v-col  cols="12" md="4" sm="10" class="pa-md-2 mb-1" offset-md="2" offset-sm="2"  v-for="(label, index) in profileInformationHeaderList" :key="index">
-          <Profile :title="label" :info="userInfo[index]"></Profile>
-        </v-col>
-      </v-row>
-
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row dense align="center" justify="center" class="fill-height">
+      <v-col cols="8">
+        <p class="blueTec--text text-center text-h4 font-weight-bold">Resumen</p>
+        <v-text-field id="Resume" label="Resumen" placeholder="" type="text"  v-model="resume" outlined dense></v-text-field>
+        <v-btn block color="blueTec" class="whiteTec--text" @click="updateResume">Actualizar</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import firebase from "firebase"
-import Profile from "@/components/Forms/Profile";
 
 export default {
   name: "ResumeComponent",
   components: {
-    Profile,
   },
   data() {
     return {
@@ -35,7 +26,8 @@ export default {
         'Carrera',
         'Correo Personal',
       ],
-      userInfo: []
+      userInfo: [],
+      resume: "",
     }
   },
   mounted() {
@@ -46,18 +38,20 @@ export default {
   methods: {
     async getStudentsInfo(user) {
 
-      const student = await firebase.firestore().collection("users").doc(user.uid).get();
-      const carrera="ITC"
-      this.userInfo.push(student.data().name);
-      this.userInfo.push(student.data().id);
-      this.userInfo.push(user.email);
-      //this.userInfo.push(student.data().Carrera);
-      this.userInfo.push(carrera);
-      this.userInfo.push(student.data().personal);
+      const student = await firebase.firestore().collection("Coaches").doc(user).get();
+      this.resume = student.data().resume;
+
     },
     getUser(){
       //console.log(firebase.auth().currentUser);
-      return firebase.auth().currentUser;
+      return firebase.auth().currentUser.email;
+    },
+    async updateResume(){
+      var user = this.getUser();
+
+      await firebase.firestore().collection("Coaches").doc(user).update({
+        resume: this.resume
+      });
     }
   }
 }
